c834ef85265067bfbcfa818dd5a533751cd39e25
Roman Anurov <anurovroman@yandex.ru>
2023-03-04 22:10:44 +0300

    auth true

---
+++ src/utils/auth.js
@@ -0,0 +1,48 @@
+export const BASE__URL = "https://auth.nomoreparties.co";
+
+export const register = (email, password) => {
+  return fetch(`${BASE__URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
+
+export const login = (email, password) => {
+  return fetch(`${BASE__URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
+
+export const chekToken = (token) => {
+  return fetch(`${BASE__URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
+};
